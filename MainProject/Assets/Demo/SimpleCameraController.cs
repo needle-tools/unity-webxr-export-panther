7cafbc36cdd50c191ec1252133e779bdfd2911d5
Marcel Wiessler <marcel@gaisterhand.de>
2021-02-09 17:01:14 +0100

    changed normal mode rotation slightly, move up down with E and Q

--- MainProject/Assets/Demo/SimpleCameraController.cs
+++ MainProject/Assets/Demo/SimpleCameraController.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
 using WebXR;
 
 namespace Demo
 {
 	public class SimpleCameraController : MonoBehaviour
 	{
-		public float RotationFactor = 30;
+		public Vector2 RotationFactor = new Vector2(30, 10);
 		public float SpeedFactor = 2;
 		public Behaviour PoseDriver;
 		public Transform Root;
 
 		private Vector2 _delta;
+
+		private void OnValidate()
+		{
+			if (!Root) Root = GetComponentInParent<XRRig>()?.transform;
+			if (!PoseDriver) TryGetComponent(out PoseDriver);
+		}
 
 		private void Update()
 		{
@@ -29,14 +37,14 @@ namespace Demo
 			{
 				var delta = mouse.delta.ReadValue();
 				if (delta.magnitude > 0)
-					_delta = Vector2.Lerp(_delta, delta, Time.deltaTime / .1f);
+					_delta = Vector2.Lerp(_delta, delta, Time.deltaTime / .5f);
 			}
 
 			if (Touchscreen.current != null)
 			{
 				var delta = Touchscreen.current.primaryTouch?.delta.ReadValue() ?? Vector2.zero;
 				if (delta.magnitude > 0)
-					_delta = Vector2.Lerp(_delta, delta, Time.deltaTime / .1f);
+					_delta = Vector2.Lerp(_delta, delta, Time.deltaTime / .5f);
 			}
 
 			Rotate(_delta);
@@ -50,6 +58,8 @@ namespace Demo
 				if (kb.sKey.isPressed) movement.z -= 1;
 				if (kb.dKey.isPressed) movement.x += 1;
 				if (kb.aKey.isPressed) movement.x -= 1;
+				if (kb.eKey.isPressed) movement.y += 1;
+				if (kb.qKey.isPressed) movement.y -= 1;
 
 				var rot = transform.localRotation.eulerAngles;
 				movement = Quaternion.Euler(0, rot.y, 0) * movement.normalized;
@@ -60,13 +70,14 @@ namespace Demo
 		private void Rotate(Vector2 delta)
 		{
 			var screenFactor = new Vector2(Screen.width, Screen.height) / Mathf.Max(Screen.dpi, 96);
-			Debug.Log(screenFactor);
-			delta *= screenFactor;
+			delta *= Mathf.Min(screenFactor.x, screenFactor.y);
+			delta.x *= 2;
 			delta = Vector2.ClampMagnitude(delta, 5);
+			delta *= RotationFactor;
 			// delta 
 			// delta *= 100;
 			var rotation = transform.localRotation.eulerAngles;
-			rotation += new Vector3(delta.y, -delta.x, 0) * (Time.deltaTime * RotationFactor);
+			rotation += new Vector3(-delta.y, -delta.x, 0) * (Time.deltaTime);
 			transform.localRotation = Quaternion.Euler(rotation);
 		}
 	}
